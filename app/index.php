600be9e04f8c4f11f9cbccf7a272d120680b56b2
Guilherme-Santt <guisant@icloud.com>
2024-02-22 12:30:41 -0300

    Inserindo opção de sair

--- app/index.php
+++ app/index.php
@@ -2,5 +2,12 @@
 session_start();
 if(!isset($_SESSION["usuario_logado"]))
     header("Location: login.php");
+
+if(isset($_GET["sair"])){
+    unset($_SESSION["usuario_logado"]);
+    header("location: login.php");
+}
+
 ?>
 <h1>Bem vindo!</h1>
+<a href="index.php?sair=true">sair</a>
